fb5cea92cee9515d35153e2e2c78439ad3c6bf0b
QiiLin <qii.lin@mail.utoronto.ca>
2020-01-27 16:20:31 -0500

    dd

--- interceptor.c
+++ interceptor.c
@@ -363,7 +363,7 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 			printk("in it 2 here\n");
 			return -EINVAL;
 		}
-
+		printk("in it 3 here\n");
 		if ((pid == 0 && current_uid() != 0) || check_pid_from_list(current->pid, pid) != 0) {
 			printk("in it 6 here\n");
 			return -EPERM;
